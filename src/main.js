3207c35f3d6127c828f2a655ff28256b46d25d42
Higor Costa <higorcosta972@gmail.com>
2022-06-10 15:52:19 -0300

    Implementa função para abrir menu

--- src/main.js
+++ src/main.js
@@ -13,3 +13,11 @@ function close() {
     let modal = document.querySelector('.modal').style.display = "none";
     let body = document.body.style.overflow = "visible";
 }
+
+const abrirMenu = document.querySelector('.abrir-menu')
+abrirMenu.addEventListener("click", openMenu)
+
+function openMenu() {
+    document.querySelector(".nav").style.width = "250px";
+    document.querySelector('.abrir-menu').style.display = 'none';
+}
