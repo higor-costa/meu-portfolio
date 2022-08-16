300bb7083caef05234bf55f66cbfc6f31d4d9d11
Higor Costa <higorcosta972@gmail.com>
2022-08-16 16:05:06 -0300

    Declara variável para receber largura do dispositivo

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,15 +1,1 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const larguraDispositivo = window.screen.width 
