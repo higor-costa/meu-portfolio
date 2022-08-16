c1e74669df1414331001e6dbb88c7751fa19f7a0
Higor Costa <higorcosta972@gmail.com>
2022-08-16 16:09:05 -0300

    Adiciona estrutura condicional para exibir 2 projetos por vez

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -17,3 +17,20 @@ if (larguraDispositivo >= 1220) {
     },
   });
 }
+else if (larguraDispositivo >= 800) {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
