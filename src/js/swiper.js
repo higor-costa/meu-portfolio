7e2f7f6103dc19683d9a948db133c6fd9591266a
Higor Costa <higorcosta972@gmail.com>
2022-09-12 16:52:01 -0300

    Altera valor da largura do dispositivo

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,6 @@
 const larguraDispositivo = window.screen.width 
 
-if (larguraDispositivo >= 1220) {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-}
-else if (larguraDispositivo >= 800) {
+if (larguraDispositivo >= 768) {
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 2,
     spaceBetween: 30,
