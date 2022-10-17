28e57b41de12a12df728c41028d83dfa8eb03e88
Higor Costa <higorcosta972@gmail.com>
2022-10-17 20:55:28 -0300

    Indenta main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict";
 
 const containerTopo = document.querySelector(".container-topo");
 const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
 function menu(controle) {
-  if (controle == 'abrir') {
-    document.querySelector('.header__modal').style.display = 'block';
-    document.querySelector('.nav').style.width = '250px';
+  if (controle == "abrir") {
+    document.querySelector(".header__modal").style.display = "block";
+    document.querySelector(".nav").style.width = "250px";
     header.appendChild(nav);
   } else {
-    document.querySelector('.nav').style.width = '0px';
-    document.querySelector('.header__modal').style.display = 'none';
+    document.querySelector(".nav").style.width = "0px";
+    document.querySelector(".header__modal").style.display = "none";
     containerTopo.appendChild(nav);
   }
 }
@@ -24,7 +24,8 @@ function menuMobile() {
   }
 }
 
-const btnHome = document.querySelector(".item")
-    .addEventListener("click", () => {
-        window.scrollTo(0, 0);
-    })
+const btnInicio = document
+  .querySelector(".item")
+  .addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
