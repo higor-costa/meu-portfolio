c330ea5853933f243e188aadb4469ae8cd361c30
Higor Costa <higorcosta972@gmail.com>
2022-10-17 21:18:47 -0300

    Define else na função quantidadeProjetos

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -22,23 +22,23 @@ function quantidadeProjetos(larguraNavegador) {
       },
     });
   } 
-else {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-}
+  else {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }
 }
 
 quantidadeProjetos(larguraJanela);
