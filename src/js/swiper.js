d319d84ec72c6f997ead1e15eb59b7e27eb34968
Higor Costa <higorcosta972@gmail.com>
2022-10-17 21:15:40 -0300

    Declara função quantidadeProjetos

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,23 +4,24 @@ window.addEventListener("resize", function () {
   quantidadeProjetos(larguraNavegador);
 });
 
-if (larguraJanela >= 768) {
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-}
+function quantidadeProjetos(larguraNavegador) {
+  if (larguraNavegador >= 768) {
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  } 
 else {
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
