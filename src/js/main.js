a27022b5e731987b0c0acc43f10bdf16acf602d8
Higor Costa <higorcosta972@gmail.com>
2022-10-24 20:25:07 -0300

    refatora main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,17 @@
 const containerTopo = document.querySelector(".container-topo");
 const nav = document.querySelector(".nav");
 const header = document.querySelector(".header");
+const headerModal = document.querySelector(".header__modal");
+
 function menu(controle) {
   if (controle == "abrir") {
-    document.querySelector(".header__modal").style.display = "block";
-    document.querySelector(".nav").style.width = "250px";
+    headerModal.style.display = "block";
+    nav.style.width = "250px";
     header.appendChild(nav);
     containerTopo.style.zIndex = "0"
   } else {
-    document.querySelector(".nav").style.width = "0px";
-    document.querySelector(".header__modal").style.display = "none";
+    nav.style.width = "0px";
+    headerModal.style.display = "none";
     containerTopo.appendChild(nav);
     containerTopo.style.zIndex = "10"
   }
@@ -37,14 +39,14 @@ function redimensionaMenu() {
   const larguraJanela = document.documentElement.clientWidth;
   if (larguraJanela > 768) {
     containerTopo.appendChild(nav);
-    document.querySelector(".nav").style.width = "40rem";
-    document.querySelector(".header__modal").style.display = "none";
+    nav.style.width = "40rem";
+    headerModal.style.display = "none";
   }
   else if (larguraJanela <= 768 && nav.style.width === '250px'){
-    document.querySelector(".nav") = "250px";
+    nav = "250px";
   }
   else {
-    document.querySelector(".nav").style.width = "0px";
+    nav.style.width = "0px";
   }
 }
 window.addEventListener("resize", redimensionaMenu);
