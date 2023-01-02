49c6156a90726a69d97525974b37c0673b693132
Higor Costa <higorcosta972@gmail.com>
2023-01-02 16:49:22 -0300

    Adiciona interatividade ao botão "Continuar lendo"

--- src/js/main.js
+++ src/js/main.js
@@ -52,3 +52,17 @@ function redimensionaMenu() {
   }
 }
 window.addEventListener('resize', redimensionaMenu);
+
+
+const botaoVerMais = document.querySelector("[data-ler-mais]");
+botaoVerMais.addEventListener("click", lerMais);
+
+function lerMais() {
+  const container = document.querySelector("[data-container-paragrafo]");
+  container.classList.toggle("ativo");
+
+  if (container.classList.contains("ativo"))
+    botaoVerMais.innerText = "Ler menos";
+  else 
+    botaoVerMais.innerText = "Continuar lendo";
+}
