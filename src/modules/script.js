c56d13dcfd26bebc8b644e4d855a15de6ac884ed
Higor Costa <higorcosta972@gmail.com>
2023-01-16 09:30:38 -0300

    Cria script.js para importar funções

---
+++ src/modules/script.js
@@ -0,0 +1,11 @@
+import animaScroll from './js/animaScroll.js';
+import controleMenu from './js/controleMenu.js';
+import lerMais from './js/lerMais.js';
+import redimensionaMenu from './js/redimensionaMenu.js';
+import scrollSuave from './js/scrollSuave.js';
+import quantidadeProjetos from './js/swiper.js';
+
+animaScroll();
+controleMenu();
+redimensionaMenu();
+scrollSuave();
