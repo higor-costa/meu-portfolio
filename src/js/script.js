551a83dab5d768a417f8a5badd0a00c5db1d5cd0
Higor Costa <higorcosta972@gmail.com>
2023-04-03 07:43:11 -0300

    Remove importação do arquivo swiper.js

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,6 @@ import controleMenu from './modules/controleMenu.js';
 import lerMais from './modules/lerMais.js';
 import redimensionaMenu from './modules/redimensionaMenu.js';
 import scrollSuave from './modules/scrollSuave.js';
-import quantidadeProjetos from './modules/swiper.js';
 
 animaScroll();
 controleMenu();
