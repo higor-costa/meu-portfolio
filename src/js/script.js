a899ac8f44ca37886f97c96c04e2ea5ff5fff8a8
Higor Costa <higorcosta972@gmail.com>
2023-04-29 11:35:54 -0300

    Importa função verMais no arquivo script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 import animaScroll from './modules/animaScroll.js';
 import controleMenu from './modules/controleMenu.js';
-import lerMais from './modules/lerMais.js';
+import verMais from './modules/verMais.js';
 import redimensionaMenu from './modules/redimensionaMenu.js';
 import scrollSuave from './modules/scrollSuave.js';
 
